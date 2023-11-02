817b8689ca0e9d699c333fca48ad0009f6c2aca7
Leonardo Mascarenhas <leonardo.pmasc@gmail.com>
2023-11-02 13:02:49 -0400

    Impl FindByID

--- customers-api/src/main/java/ca/vanier/customersapi/controller/CustomerController.java
+++ customers-api/src/main/java/ca/vanier/customersapi/controller/CustomerController.java
@@ -1,9 +1,11 @@
 package ca.vanier.customersapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.vanier.customersapi.entity.Customer;
@@ -16,14 +18,18 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    // Save
     @PostMapping("/save")
     public void save(@RequestBody Customer customer) {
 
         customerService.save(customer);
 
         System.out.println("HELLO");
-
     }
+
+    @GetMapping("/find")
+    public Customer getCustomer(@RequestParam Long id) {
+        return customerService.findById(id).get();
+    }
+
     
 }
