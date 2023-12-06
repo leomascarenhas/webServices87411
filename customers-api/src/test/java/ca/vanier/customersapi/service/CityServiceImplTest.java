2f41d150410f84e30da309fb82714df2ab45a980
Karishma <69646809+Karishma0407@users.noreply.github.com>
2023-12-06 14:13:49 -0500

    test update and delete methods

--- customers-api/src/test/java/ca/vanier/customersapi/service/CityServiceImplTest.java
+++ customers-api/src/test/java/ca/vanier/customersapi/service/CityServiceImplTest.java
@@ -82,11 +82,50 @@ class CityServiceImplTest {
 
     @Test
     void update() {
+         // setup
+        Country canada = new Country();
+        canada.setName("Canada");
 
+        City cityToSave = new City();
+        cityToSave.setId(1L);
+        cityToSave.setName("Montreal");
+        cityToSave.setCountry(canada);
+
+        City cityUpdated = new City();
+        cityUpdated.setId(1L);
+        cityUpdated.setName("Toronto");
+        cityUpdated.setCountry(canada);
+
+        Mockito
+                .doNothing()
+                .when(cityService).update(1L);
+
+        // Execution
+        cityService.update(1L);
+
+        // Assertions
+        assert cityUpdated.getName().equals("Toronto");
     }
 
     @Test
     void delete() {
+         // setup
+        Country canada = new Country();
+        canada.setName("Canada");
 
+        City cityToDelete = new City();
+        cityToDelete.setId(1L);
+        cityToDelete.setName("Toronto");
+        cityToDelete.setCountry(canada);
+
+        Mockito
+                .doNothing()
+                .when(cityService).delete(1L);
+
+        // Execution
+        cityService.delete(1L);
+
+        // Assertions
+        Mockito.verify(cityService, Mockito.times(1)).delete(1L);
     }
 }
